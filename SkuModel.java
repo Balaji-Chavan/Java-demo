e7d3460da84910d38a5af566bb51859c70d2fdf5
Balaji-Chavan <136039701+Balaji-Chavan@users.noreply.github.com>
2023-06-09 10:22:02 +0530

    Add files via upload

---
+++ SkuModel.java
@@ -0,0 +1,33 @@
+package com.cjss.model;
+
+public class SkuModel {
+	  private String skuId;
+	    private String skuSize;
+	    private Float price;
+	    
+	    
+		public SkuModel(String skuId, String skuSize, Float price) {
+			super();
+			this.skuId = skuId;
+			this.skuSize = skuSize;
+			this.price = price;
+		}
+		public String getSkuId() {
+			return skuId;
+		}
+		public void setSkuId(String skuId) {
+			this.skuId = skuId;
+		}
+		public String getSkuSize() {
+			return skuSize;
+		}
+		public void setSkuSize(String skuSize) {
+			this.skuSize = skuSize;
+		}
+		public Float getPrice() {
+			return price;
+		}
+		public void setPrice(Float price) {
+			this.price = price;
+		}
+}
